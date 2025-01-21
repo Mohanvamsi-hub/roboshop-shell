4f3bff172efb649ac35d2a05b486d617f2dc3190
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-01-21 08:22:44 +0530

    cp command

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,6 @@
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 dnf install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
