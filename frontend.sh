aa0c157848065a4b550010741a5e53da7498120a
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-01-21 08:13:34 +0530

    frontend initial

---
+++ frontend.sh
@@ -0,0 +1,11 @@
+cp frontend.conf /etc/nginx/default.d/roboshop.conf
+dnf install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl start nginx
+systemctl enable nginx
+
+
+
