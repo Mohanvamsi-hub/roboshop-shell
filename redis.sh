96f49fdf4416006f4db9ad8a875a64089d096e8d
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-01-29 09:29:17 +0530

    redis server

--- redis.sh
+++ redis.sh
@@ -1,12 +1,13 @@
 echo -e "\e[36m>>>>>>>>>>>Installing package for redis<<<<<<<<<<<\e[0m"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-echo -e "\e[36m>>>>>>>>>>>Enabling Redis version<<<<<<<<<<<\e[0m"
-dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m>>>>>>>>>>>Installing redis<<<<<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
 dnf install redis -y
+
 echo -e "\e[36m>>>>>>>>>>>Changing port listen address<<<<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+
 echo -e "\e[36m>>>>>>>>>>>Starting redis service<<<<<<<<<<<\e[0m"
 systemctl enable redis
 systemctl restart redis
