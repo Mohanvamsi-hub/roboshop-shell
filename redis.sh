afcbde1822ec6f8a606693719d3c510a1fa2821c
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-01-29 09:06:54 +0530

    redis server

--- redis.sh
+++ redis.sh
@@ -1,6 +1,10 @@
+echo -e "\e[36m>>>>>>>>>>>Installing package for redis<<<<<<<<<<<\e[0m"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-dnf install redis -y
 
+echo -e "\e[36m>>>>>>>>>>>Enabling Redis version<<<<<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
+echo -e "\e[36m>>>>>>>>>>>Installing redis<<<<<<<<<<<\e[0m"
+dnf install redis -y
+echo -e "\e[36m>>>>>>>>>>>Starting redis service<<<<<<<<<<<\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
