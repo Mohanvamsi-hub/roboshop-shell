1b82cf987aaebf13cb2e4a129e7f16f138f2bbcb
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-09 03:17:39 +0530

    mongo changes

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[36m>>>>>>>>>>>  Installing mongodb  <<<<<<<<<<<<<\e[0m"
 dnf install mongodb-org -y
 
 echo -e "\e[36m>>>>>>>>>>>  Updating port listen address  <<<<<<<<<<<<<\e[0m"
-sed "s|127.0.0.1|0.0.0.0|" /etc/mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
 echo -e "\e[36m>>>>>>>>>>>  Enabling mongo service  <<<<<<<<<<<<<\e[0m"
 systemctl enable mongod
