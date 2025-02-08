e26851aaed5df9712b8c7709f60de319156855dc
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-09 03:30:47 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,9 @@ dnf install nodejs -y
 
 echo -e "\e[36m>>>>>>>>>>>  Adding roboshop user  <<<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>  Adding roboshop user  <<<<<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
 
 echo -e "\e[36m>>>>>>>>>>>  Downloading zip file  <<<<<<<<<<<<<\e[0m"
@@ -23,6 +26,7 @@ echo -e "\e[36m>>>>>>>>>>>  copying service file  <<<<<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[36m>>>>>>>>>>>  Enabling catalogue service  <<<<<<<<<<<<<\e[0m"
+systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
