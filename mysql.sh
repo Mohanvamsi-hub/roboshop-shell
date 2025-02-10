8ce5d9ca6143629109c3b9e7be28c72291dfeeef
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-10 09:42:39 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,27 @@
+echo -e "\e[36m>>>>>>>>>>>  Disabling old version of MySQL  <<<<<<<<<<<<<\e[0m"
+dnf module disable mysql -y
+
+echo -e "\e[36m>>>>>>>>>>>  Copying repo SQL file  <<<<<<<<<<<<<\e[0m"
+cp /home/centos/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>>>  Installing mysql  <<<<<<<<<<<<<\e[0m"
+dnf install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>>>>  starting mysql service  <<<<<<<<<<<<<\e[0m"
+systemctl enable mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m>>>>>>>>>>>  setting password  <<<<<<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
+
+
+
+
+
+
+
+
+
+
+
+
