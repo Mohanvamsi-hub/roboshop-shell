0d08cf5713323c187a0d8265fe8a3f845d38f6cc
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-10 09:22:51 +0530

    cart

---
+++ cart.sh
@@ -0,0 +1,36 @@
+echo -e "\e[36m>>>>>>>>>>>  Disabling old version and enabling new  <<<<<<<<<<<<<\e[0m"
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+
+echo -e "\e[36m>>>>>>>>>>>  Installing Nodejs  <<<<<<<<<<<<<\e[0m"
+dnf install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>>>  Adding roboshop user  <<<<<<<<<<<<<\e[0m"
+useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>  Adding roboshop user  <<<<<<<<<<<<<\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>>  Downloading zip file  <<<<<<<<<<<<<\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+
+echo -e "\e[36m>>>>>>>>>>>  Unzipping file contents  <<<<<<<<<<<<<\e[0m"
+unzip /tmp/cart.zip
+
+echo -e "\e[36m>>>>>>>>>>>  Installing NPM dependenceis  <<<<<<<<<<<<<\e[0m"
+npm install
+
+echo -e "\e[36m>>>>>>>>>>>  copying service file  <<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+cd
+echo -e "\e[36m>>>>>>>>>>>  Enabling catalogue service  <<<<<<<<<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable cart
+systemctl restart cart
+
+
+
+
