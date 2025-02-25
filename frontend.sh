e60c163d57883ecf52cc1b04329a946bdb07acc2
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-25 07:54:20 +0530

    frontend changes

--- frontend.sh
+++ frontend.sh
@@ -2,22 +2,33 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source $script_path/common.sh
 
-echo -e "\e[36m>>>>>>>>>>>  Installing nginx  <<<<<<<<<<<<<\e[0m"
-dnf install nginx -y
+print_heading "Installing nginx"
+dnf install nginx -y &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Copying conf file  <<<<<<<<<<<<<\e[0m"
-cp roboshop.conf  /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
+print_heading "Copying configuration file"
+cp roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Download ZIP file  <<<<<<<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+print_heading "Removing the old content if exist"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  unzip file  <<<<<<<<<<<<<\e[0m"
-unzip /tmp/frontend.zip
+print_heading "Download ZIP file"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Enabling service  <<<<<<<<<<<<<\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+cd /usr/share/nginx/html &>>$log_file
+
+
+print_heading "Unzipping zip file"
+unzip /tmp/frontend.zip &>>$log_file
+function_stat_check $?
+
+print_heading "Enabling service"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+function_stat_check $?
+
 
 
