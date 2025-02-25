2a75fd095b8f757c809a3140144b2b0ee0739be0
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-25 08:11:39 +0530

    mongo changes

--- mongodb.sh
+++ mongodb.sh
@@ -2,15 +2,26 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source $script_path/common.sh
 
-echo -e "\e[36m>>>>>>>>>>>  Copying repo file  <<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+print_heading "Copying repo file"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Installing mongodb  <<<<<<<<<<<<<\e[0m"
-dnf install mongodb-org -y
+print_heading "Installing mongodb"
+dnf install mongodb-org -y &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Updating port listen address  <<<<<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+print_heading "Updating port listen address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Enabling mongo service  <<<<<<<<<<<<<\e[0m"
-systemctl enable mongod
-systemctl restart mongod
+print_heading "Enabling mongo service"
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
+function_stat_check $?
+
+
+
+
+
+
+
