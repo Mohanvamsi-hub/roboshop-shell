925a1c482a16d23612dfef5ed3c8da19f96db933
Mohan Vamsi <mohanvamsi107@gmail.com>
2025-02-25 09:09:56 +0530

    redis changes

--- redis.sh
+++ redis.sh
@@ -2,22 +2,27 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source $script_path/common.sh
 
-echo -e "\e[36m>>>>>>>>>>>  Installing redis repo  <<<<<<<<<<<<<\e[0m"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.9.rpm -y
 
-echo -e "\e[36m>>>>>>>>>>>  Enabling redis 6.2  <<<<<<<<<<<<<\e[0m"
-dnf module enable redis:remi-6.2 -y
+print_heading "Installing redis repo"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.9.rpm -y &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Installing redis  <<<<<<<<<<<<<\e[0m"
-dnf install redis -y
+print_heading "Enabling redis 6.2"
+dnf module enable redis:remi-6.2 -y &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  change port address  <<<<<<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+print_heading "Installing redis"
+dnf install redis -y &>>$log_file
+function_stat_check $?
 
-echo -e "\e[36m>>>>>>>>>>>  Enabling redis service  <<<<<<<<<<<<<\e[0m"
-systemctl enable redis
-systemctl restart redis
+print_heading "change port address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$log_file
+function_stat_check $?
 
+print_heading "Enabling redis service"
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
+function_stat_check $?
 
 
 
